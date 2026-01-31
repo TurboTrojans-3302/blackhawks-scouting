d125b81be2aa6eaeb2c51a0b88b57b0b10c5c41d
Moosterbox <pesto.ancho234@gmail.com>
2026-01-31 13:46:39 -0500

    Removed "Upload to drive button"

--- src/components/InspectorContainer.vue
+++ src/components/InspectorContainer.vue
@@ -9,14 +9,6 @@
       </select>
       <button @click="deleteData">Delete</button>
       <button @click="downloadData">Download</button>
-      <button @click="dummyupload" >Upload To Drive
-        <div id="g_id_onload"
-          data-client_id="344124639348-l0ld2jh9h6no4ja3mpei30j6b9nn6o8h.apps.googleusercontent.com"
-          data-login_uri="https://turbotrojans-3302.github.io"
-          data-your_own_param_1_to_login="5"
-          data-your_own_param_2_to_login="5">
-        </div>
-      </button>
       <button @click="clearData">Clear All</button>
     </template>
   </div>
@@ -39,7 +31,6 @@ import fs from "node:fs";
 
 const widgets = useWidgetsStore();
 let selectedIdx = $ref(0); // The index of the entry selected in the combobox
-const folderId = "1HyC6zKH98n0OzDhmhKWJkupdxY-Knd1k";
 const downloadLink = $ref<HTMLAnchorElement>();
 const selectedRecords = $ref(new Set<number>());
 const hasSelectedRecords = $computed(() => selectedRecords.size > 0);
@@ -76,63 +67,7 @@ function downloadData() {
 }
 
 
-function dummyupload(){
-  uploadToDrive(folderId);
-}
-// uploads CSV files to Google Drive(does nothing for now, sorry!)
-// Abandonded until further notice
-// link to example: https://developers.google.com/drive/api/guides/folder#node.js
-// link to stackoverflow question: https://stackoverflow.com/questions/51584732/create-folder-and-upload-file-to-google-drive-from-typescript-cannot-compile
-// link to node.js blog: https://nodejs.org/en/blog/announcements/v22-release-announce
-/*
-async function uploadToDrive(folderId){
 
- console.log("messing up here?");
-
- if (selectedEntry === undefined) return;
-  if (downloadLink === undefined) return; // Make sure the link exists
-  downloadLink.href = widgets.makeDownloadLink({ header: selectedEntry.header, values: filterRecords(true) });
-
-
-  const{GoogleAuth} = await import('google-auth-library');
- const{google} = await import("googleapis")
-
-  // get credentials and build service
-  // get proper auth mechanism?(not sure what that means, look up later)
-  const auth = new GoogleAuth({scopes: "https://www.googleapis.com/auth/drive.file"});
-  const service = google.drive({version: "v3", auth});
-
-  //set folderId and upload csv
-  folderId = "1HyC6zKH98n0OzDhmhKWJkupdxY-Knd1k";
-
-  // copied code from downladData finction
-
-
-  const fileMetadata = {
-    Name: selectedEntry.header,
-    parents: [folderId]
-  };
-
-  const media = {
-    mimeType: "text/csv",
-    body: fs.createReadStream(`files/${downloadLink.href}`),
-  };
-
-  try {
-    const file = await service.files.create({
-      requestBody: fileMetadata,
-      media: media,
-      fields: "id",
-    });
-    console.log("File Id:", file.data.id);
-    return file.data.id;
-  } catch (err) {
-    // TODO(developer) - Handle error
-    throw err;
-  }
-
-}
-*/
 function clearData() {
   if (!confirm("Clear all saved entries in local storage permanently?")) return;
 
